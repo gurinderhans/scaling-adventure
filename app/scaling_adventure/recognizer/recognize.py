b5e7b5a1f201e289e4685c3edd559756fc8195ef
Gurinder Hans <hello@gurinderhans.me>
2015-07-14 23:07:35 -0700

    create image recognizer module, more cleanup

---
+++ app/scaling_adventure/recognizer/recognize.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import cv2
+import numpy as np
+
+class Recognizer(object):
+    """ docstring for Recognizer """
+    def __init__(self, directory_path, compareFile, compareThreshold):
+        super(Recognizer, self).__init__()
+        self.path = directory_path
+        self.file = compareFile
+        self.threshold = float(compareThreshold)
+
+    """_"""
+    def compareWith(self, eigenModelFile):
+        model = cv2.createEigenFaceRecognizer(threshold=self.threshold)
+
+        # Load the model
+        model.load(eigenModelFile)
+
+        # Read the image we're looking for
+        sampleImage = cv2.imread(self.file, cv2.IMREAD_GRAYSCALE)
+        sampleImage = cv2.resize(sampleImage, (256,256))
+
+        # Look through the model and find the face it matches
+        model_prediction = model.predict(sampleImage)
+
+        [p_label, p_confidence] = model_prediction
+
+        # Print the confidence levels
+        print "Predicted label = {0} (confidence={1})".format(p_label, p_confidence)
+
+        # hold all the found images paths
+        result_paths = []
+
+        # If the model found something, print the file path
+        if (p_label > -1):
+            count = 0
+            for dirname, dirnames, filenames in os.walk(self.path):
+                for subdirname in dirnames:
+                    subject_path = os.path.join(dirname, subdirname)
+                    if (count == p_label):
+                        for filename in os.listdir(subject_path):
+                            result_paths.append(subject_path)
+
+                    count = count+1
+
+        return result_paths + model_prediction
+
+
+# [SAMPLE USAGE]
+# recognizer = Recognizer(os.path.abspath('eigen_models/'), 'test.jpg', 1000)
+# recognizer.compareWith('eigenModel_1.xml')
--- app/scaling_adventure/routes.py
+++ app/scaling_adventure/routes.py
@@ -1,12 +1,15 @@
 import os
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug import secure_filename
+from recognizer.recognize import Recognizer
 
-UPLOAD_FOLDER = '/'
+UPLOAD_FOLDER = '/Users/ghans/Documents/Tate-Hack/app/scaling_adventure/uploads/' # must be absolute
+EIGEN_MODELS_FOLDER = '/' # must be absolute
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['EIGEN_MODELS_FOLDER'] = EIGEN_MODELS_FOLDER
 
 #
 def allowed_file(filename):
@@ -18,14 +21,21 @@ def home():
   return render_template('index.html')
 
 """
-Upload the given file to `UPLOAD_FOLDER`
+- Upload the given file to `UPLOAD_FOLDER`
+- Then compare image and send back to client
 """
 @app.route('/upload', methods= ['POST'])
 def upload_file():
     file = request.files['file']
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        fileSavePath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(fileSavePath)
+
+        # TODO: processing here....
+        # recognizer = Recognizer(app.config['EIGEN_MODELS_FOLDER'], fileSavePath, 1000)
+        # recognizer.compareWith('eigenModel_1.xml')
+
         return str(1)
     return str(0)
 
